d383ec60956c7816bc2b77e9980e983282fc5bc5
Frank Starren <frank.starren@gmail.com>
2025-06-04 16:06:01 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,31 @@
 
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { RouterProvider } from "react-router-dom";
+import { AppProvider } from "./context/AppContext";
+import { AuthProvider } from "./context/AuthContext";
+import router from "../src/router.jsx"; 
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        bg: "gray.50",
+        color: "gray.800",
+      },
+    },
+  },
+});
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <AuthProvider>
+        <AppProvider>
+          <RouterProvider router={router} />
+        </AppProvider>
+      </AuthProvider>
+    </ChakraProvider>
+  </React.StrictMode>
+);
